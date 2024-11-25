4ff16be704e5ccab2441aa6252310627c556669d
Tinh Nguyen <tinh62603@gmail.com>
2024-11-24 20:40:32 -0700

    question 2

--- Sample.java
+++ Sample.java
@@ -6,64 +6,34 @@ import java.util.Collections;
 public class Sample {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> arr = new ArrayList<Integer>(1000000);
-		for (int i = 0; i < 1000000; i++) {
-			double ran = Math.random();
-			if (ran <= .35) {
-				arr.add(i, 1);
-			}
-			if (ran > .35 && ran <= .75) {
-				arr.add(i, -1);
-			}
-
-			if (ran > .75) {
-				arr.add(i, 0);
-			}
-		}
-		int one = 0;
-		int zero = 0;
-		int negative = 0;
-		for (int i = 0; i < arr.size(); i++) {
-			if (arr.get(i) == 0) {
-				zero++;
-			} else if (arr.get(i) == 1) {
-				one++;
-			} else if (arr.get(i) == -1) {
-				negative++;
-			}
-		}
-		System.out.println("one: " + one + " zero: " + zero + " negative: " + negative);
-
-		
-		for (int k = 0; k < 100; k++) {
-			int negativeMajority = 0;
-			for (int i = 0; i < 200; i++) {
-				int onec = 0;
-				int zeroc = 0;
-				int negativec = 0;
-				Collections.shuffle(arr);
-				// change sample size
-				for (int j = 0; j < 700; j++) {
-					if (arr.get(j) == 0) {
-						zeroc++;
-					} else if (arr.get(j) == 1) {
-						onec++;
-					} else if (arr.get(j) == -1) {
-						negativec++;
+		double p = .5;
+		int t = 16 * 10000;
+		int crosses = 0;
+		for (int j = 0; j < 50; j++) {
+			int pos = 0;
+			Boolean negative = false;
+			int cross = 0;
+			for (int i = 0; i < t; i++) {
+				double ran = Math.random();
+				if (ran > p) {
+					pos += 1;
+					if (negative == true && pos == 1) {
+						if (t != 0)
+							cross++;
+						negative = false;
+					}
+				} else {
+					pos -= 1;
+					if (negative == false && pos == -1) {
+						if (t != 0)
+							cross++;
+						negative = true;
 					}
 				}
-				//System.out.println("one: " + onec + " zero: " + zeroc + " negative: " + negativec);
-				//System.out.println(Math.max(onec, Math.max(zeroc, negativec)));
-				if (negativec > zeroc && negativec > onec) {
-					negativeMajority++;
-				}
 			}
-			if ((double) negativeMajority / 200 < .9) {
-				System.out.println("Negative majority: " + negativeMajority + "/200");
-				//System.out.println("no");
-			}
+			crosses+=cross;
 		}
-		System.out.println("done");
-	
+		System.out.println("cross" + crosses);
+		System.out.println("cross" + crosses/50);
 	}
 }
